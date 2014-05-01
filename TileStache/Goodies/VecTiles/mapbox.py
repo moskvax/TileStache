624318a4997bcb0c7082eac14784da1a90bd3a69
Harish Krishna <harish.krsn@gmail.com>
2014-05-01 11:13:38 -0400

    Manual zig zag encoding + geometry encoding (first pass)

--- TileStache/Goodies/VecTiles/mapbox.py
+++ TileStache/Goodies/VecTiles/mapbox.py
@@ -3,6 +3,8 @@ from Mapbox import vector_tile_pb2
 from Mapbox.GeomEncoder import GeomEncoder
 from shapely.wkb import loads
 
+from math import floor, fabs
+
 from TileStache.Core import KnownUnknown
 import re
 import logging
@@ -10,6 +12,9 @@ import struct
 
 # coordindates are scaled to this range within tile
 extents = 4096
+cmd_bits = 3
+path_multiplier = 16
+tolerance = 0
 
 def decode(file):
     ''' Stub function to decode a mapbox vector tile file into a list of features.
@@ -46,57 +51,92 @@ class VectorTile:
     """
     def __init__(self, extents, layer_name=""):
         self.geomencoder   = GeomEncoder(extents)
-
-        self.tile          = vector_tile_pb2.tile()       
-        self.feature_count = 0
-        self.keys   = []
-        self.values = []
-        self.pixels = []
-
+        self.tile          = vector_tile_pb2.tile()
 
     def addFeatures(self, features, coord, extents, layer_name=""):
         self.layer         = self.tile.layers.add()
         self.layer.name    = layer_name
         self.layer.version = 2
         self.layer.extent  = extents
+        self.feature_count = 0
+        self.keys   = []
+        self.values = []
+        self.pixels = []
         for feature in features:
             self.addFeature(feature, coord)
 
     def addFeature(self, feature, coord):
         geom = self.geomencoder
-        
+        x_ = coord.column
+        y_ = coord.row
+        cmd= 1
+        skipped_last = False
+
         f = self.layer.features.add()
         self.feature_count += 1
         f.id = self.feature_count
-        
+        f.type = self.tile.Point if geom.isPoint else (self.tile.Polygon if geom.isPoly else self.tile.LineString)
+
         self._handle_attr(self.layer, f, feature[1])
 
         geom.parseGeometry(feature[0])
+        coordinates = []
+        for coords in self._chunker(geom.coordinates,2):
+            coordinates.append((coords[0], coords[1]))
+        length = geom.num_points
 
-        if geom.isPoint:
-            f.type = self.tile.Point
-        else:
-            # # empty geometry
-            # if len(geom.index) == 0:
-            #     logging.debug('empty geom: %s %s' % feature[1])
-            #     return
+        f.geometry.append(self._encode_cmd_length(1, length))
 
-            if geom.isPoly:
-                f.type = self.tile.Polygon
+        it = 0
+        cmd= 1 if geom.isPoint else 2 # TODO: figure out if cmd can change within a feature geom
+
+        for coordinate in coordinates:
+            x,y = coordinate[0],coordinate[1]
+
+            cur_x = int(floor((x * path_multiplier) + 0.5))
+            cur_y = int(floor((y * path_multiplier) + 0.5))
+
+            if skipped_last and cmd == 1:
+                self._handle_skipped_last(f, cur_x, cur_y, x_, y_)
+
+            dx = cur_x - x
+            dy = cur_y - y
+
+            sharp_turn_ahead = False
+
+            if (it+2 <= len(coordinates)):
+                next_coord = coordinates[it+1]
+                next_x, next_y = next_coord[0], next_coord[1]
+                next_dx = fabs(cur_x - int(floor((next_x * path_multiplier) + 0.5)))
+                next_dy = fabs(cur_y - int(floor((next_y * path_multiplier) + 0.5)))
+                if ((next_dx == 0 and next_dy >= tolerance) or (next_dy == 0 and next_dx >= tolerance)):
+                    sharp_turn_ahead = True
+
+
+            if (sharp_turn_ahead or fabs(dx) >= tolerance or fabs(dy) >= tolerance):
+                f.geometry.append((dx << 1) ^ (dx >> 31))
+                f.geometry.append((dy << 1) ^ (dy >> 31))
+                x_ = cur_x
+                y_ = cur_y
+                skipped_last = False
             else:
-                f.type = self.tile.LineString
+                skipped_last = True
+            it = it+1
 
-            # add coordinate index list (coordinates per geometry)
-            # feature.indices.extend(geom.index)
+        f.geometry.append(self._encode_cmd_length(7, length))
 
-            # add indice count (number of geometries)
-            # if len(feature.indices) > 1:
-            #     feature.num_indices = len(feature.indices)
+    def _encode_cmd_length(self, cmd, length):
+        # cmd: 1 (MOVE_TO)
+        # cmd: 2 (LINE_TO)
+        # cmd: 7 (CLOSE_PATH)
+        return (length << cmd_bits) | (cmd & ((1 << cmd_bits) - 1))
 
-        # add coordinates
-        for coordinate in geom.coordinates:
-            if coordinate <= 4096 and coordinate > 0:
-                f.geometry.append(coordinate)
+    def _chunker(self, seq, size):
+        return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+
+    def _handle_skipped_last(self, f, cur_x, cur_y, x_, y_):
+        # TODO
+        return True
 
     def _handle_attr(self, layer, feature, props):
         for k,v in props.items():
