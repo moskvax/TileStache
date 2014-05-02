93d345970f347577d5fda3d898e29fa7dfc588c9
Harish Krishna <harish.krsn@gmail.com>
2014-05-02 11:58:39 -0400

    re-writing the encoding with while loops. handle unicode encoding str values.

--- TileStache/Goodies/VecTiles/mapbox.py
+++ TileStache/Goodies/VecTiles/mapbox.py
@@ -16,6 +16,10 @@ cmd_bits = 3
 path_multiplier = 16
 tolerance = 0
 
+CMD_MOVE_TO = 1
+CMD_LINE_TO = 2
+CMD_SEG_END = 7
+
 def decode(file):
     ''' Stub function to decode a mapbox vector tile file into a list of features.
     
@@ -69,8 +73,15 @@ class VectorTile:
         geom = self.geomencoder
         x_ = coord.column
         y_ = coord.row
-        cmd= 1
+        cmd= -1
+        cmd_idx = -1
+        vtx_cmd = -1
+        prev_cmd= -1
+        
+        skipped_index = -1
         skipped_last = False
+        cur_x = 0
+        cur_y = 0
 
         f = self.layer.features.add()
         self.feature_count += 1
@@ -83,47 +94,95 @@ class VectorTile:
         coordinates = []
         for coords in self._chunker(geom.coordinates,2):
             coordinates.append((coords[0], coords[1]))
-        length = geom.num_points
-
-        f.geometry.append(self._encode_cmd_length(1, length))
-
+        
         it = 0
-        cmd= 1 if geom.isPoint else 2 # TODO: figure out if cmd can change within a feature geom
+        length = 0
+        
+        while (True):
+            if it >= len(coordinates):
+                break;
+            
+            x,y = coordinates[it][0],coordinates[it][1]
+            
+            vtx_cmd= self._get_cmd_type(f.type, geom.num_points, it)
+            
+            if vtx_cmd != cmd:
+                if (cmd_idx >= 0):
+                    f.geometry.__setitem__(cmd_idx, self._encode_cmd_length(cmd, length))
 
-        for coordinate in coordinates:
-            x,y = coordinate[0],coordinate[1]
+                cmd = vtx_cmd
+                length = 0
+                cmd_idx = len(f.geometry)
+                f.geometry.append(0) #placeholder added in first pass
 
-            cur_x = int(floor((x * path_multiplier) + 0.5))
-            cur_y = int(floor((y * path_multiplier) + 0.5))
+            if (vtx_cmd == CMD_MOVE_TO or vtx_cmd == CMD_LINE_TO):
+                if cmd == CMD_MOVE_TO and skipped_last and skipped_index >1:
+                    self._handle_skipped_last(f, skipped_index, cur_x, cur_y, x_, y_)
+                
+                # Compute delta to the previous coordinate.
+                cur_x = int(floor((x * path_multiplier) + 0.5))
+                cur_y = int(floor((y * path_multiplier) + 0.5))
 
-            if skipped_last and cmd == 1:
-                self._handle_skipped_last(f, cur_x, cur_y, x_, y_)
+                dx = cur_x - x_
+                dy = cur_y - y_
+                
+                sharp_turn_ahead = False
 
-            dx = cur_x - x
-            dy = cur_y - y
+                if (it+2 <= len(coordinates)):
+                    next_coord = coordinates[it+1]
+                    if self._get_cmd_type(f.type, geom.num_points, it) == CMD_LINE_TO:
+                        next_x, next_y = next_coord[0], next_coord[1]
+                        next_dx = fabs(cur_x - int(floor((next_x * path_multiplier) + 0.5)))
+                        next_dy = fabs(cur_y - int(floor((next_y * path_multiplier) + 0.5)))
+                        if ((next_dx == 0 and next_dy >= tolerance) or (next_dy == 0 and next_dx >= tolerance)):
+                            sharp_turn_ahead = True
 
-            sharp_turn_ahead = False
-
-            if (it+2 <= len(coordinates)):
-                next_coord = coordinates[it+1]
-                next_x, next_y = next_coord[0], next_coord[1]
-                next_dx = fabs(cur_x - int(floor((next_x * path_multiplier) + 0.5)))
-                next_dy = fabs(cur_y - int(floor((next_y * path_multiplier) + 0.5)))
-                if ((next_dx == 0 and next_dy >= tolerance) or (next_dy == 0 and next_dx >= tolerance)):
-                    sharp_turn_ahead = True
-
-
-            if (sharp_turn_ahead or fabs(dx) >= tolerance or fabs(dy) >= tolerance):
-                f.geometry.append((dx << 1) ^ (dx >> 31))
-                f.geometry.append((dy << 1) ^ (dy >> 31))
-                x_ = cur_x
-                y_ = cur_y
-                skipped_last = False
+                # Keep all move_to commands, but omit other movements that are
+                # not >= the tolerance threshold and should be considered no-ops.
+                # NOTE: length == 0 indicates the command has changed and will
+                # preserve any non duplicate move_to or line_to
+                if length == 0 or sharp_turn_ahead or fabs(dx) >= tolerance or fabs(dy) >= tolerance:
+                    # Manual zigzag encoding.
+                    f.geometry.append((dx << 1) ^ (dx >> 31))
+                    f.geometry.append((dy << 1) ^ (dy >> 31))
+                    x_ = cur_x
+                    y_ = cur_y
+                    skipped_last = False
+                    length = length + 1
+                else:
+                    skipped_last = True
+                    skipped_index = len(f.geometry)
+            elif vtx_cmd == CMD_SEG_END:
+                if prev_cmd != CMD_SEG_END:
+                    length = length + 1
             else:
-                skipped_last = True
-            it = it+1
+                raise Exception("Unknown command type: '%s'" % vtx_cmd)
+            
+            it = it + 1
+            prev_cmd = cmd
 
-        f.geometry.append(self._encode_cmd_length(7, length))
+        # at least one vertex + cmd/length
+        if (skipped_last and skipped_index > 1): 
+            # if we skipped previous vertex we just update it to the last one here.
+            handle_skipped_last(f, skipped_index, cur_x, cur_y, x_, y_)
+        
+        # Update the last length/command value.
+        if (cmd_idx >= 0):
+            f.geometry.__setitem__(cmd_idx, self._encode_cmd_length(cmd, length))
+
+
+    # TODO: figure out if cmd can change within a feature geom
+    def _get_cmd_type(self, gtype, gpoints, glen):
+        cmd_type = -1
+        if gtype == self.tile.Point:
+            cmd_type = CMD_MOVE_TO
+        elif gtype == self.tile.Polygon or gtype == self.tile.LineString:
+            cmd_type = CMD_LINE_TO
+        if glen==0:
+            cmd_type = CMD_MOVE_TO
+        if gtype == self.tile.Polygon and glen>=gpoints:
+            cmd_type = CMD_SEG_END
+        return cmd_type
 
     def _encode_cmd_length(self, cmd, length):
         # cmd: 1 (MOVE_TO)
@@ -134,9 +193,17 @@ class VectorTile:
     def _chunker(self, seq, size):
         return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
-    def _handle_skipped_last(self, f, cur_x, cur_y, x_, y_):
-        # TODO
-        return True
+    def _handle_skipped_last(self, f, skipped_index, cur_x, cur_y, x_, y_):
+        last_x = f.geometry[skipped_index - 2]
+        last_y = f.geometry[skipped_index - 1]
+        last_dx = ((last_x >> 1) ^ (-(last_x & 1)))
+        last_dy = ((last_y >> 1) ^ (-(last_y & 1)))
+        dx = cur_x - x_ + last_dx
+        dy = cur_y - y_ + last_dy
+        x_ = cur_x
+        y_ = cur_y
+        f.geometry.__setitem__(skipped_index - 2, ((dx << 1) ^ (dx >> 31)))
+        f.geometry.__setitem__(skipped_index - 1, ((dy << 1) ^ (dy >> 31)))
 
     def _handle_attr(self, layer, feature, props):
         for k,v in props.items():
@@ -154,7 +221,7 @@ class VectorTile:
                     val.bool_value = v
                 elif (isinstance(v,str)) or (isinstance(v,unicode)):
                     val = layer.values.add()
-                    val.string_value = v
+                    val.string_value = unicode(v,'utf8')
                 elif (isinstance(v,int)):
                     val = layer.values.add()
                     val.int_value = v
